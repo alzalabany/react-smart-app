e2d91e9e15d9700b718c1837409244c0e97bc9a7
Momen Zalabany <m.zalabany@pangea-technology.com>
2020-12-20 18:03:32 +0200

    feat: 🎸 added cypress for e2e
    no ci configuration for cypress yet, but it can be used localy

---
+++ cypress/integration/spec.js
@@ -0,0 +1,22 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="cypress" />
+
+context('Example Cypress TodoMVC test', () => {
+  beforeEach(() => {
+    cy.visit('http://todomvc.com/examples/vue/')
+  })
+
+  it('adds 2 todos', function () {
+    cy.get('.new-todo')
+      .type('learn testing{enter}')
+      .type('be cool{enter}')
+    cy.get('.todo-list li').should('have.length', 2)
+  })
+
+  // more examples
+  //
+  // https://github.com/cypress-io/cypress-example-todomvc
+  // https://github.com/cypress-io/cypress-example-kitchensink
+  // https://on.cypress.io/writing-your-first-test
+})
